15eb9a57cd9c8c3e4858f472763ad06aa2f3d76c
Stacey Ferreira <staceyferreira10@gmail.com>
2019-02-06 12:00:04 +0530

    Changes to the machine code API

--- app/Http/Controllers/MachineMasterController.php
+++ app/Http/Controllers/MachineMasterController.php
@@ -5,13 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Dingo\Api\Routing\Helpers;
 use App\MachineMaster;
+use Illuminate\Support\Facades\DB;
 
 class MachineMasterController extends Controller
 {
     use Helpers;
 
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
     public function getMachineCode()
     {
+        $database = $this->setDatabaseConfig($this->request);
+        DB::setDefaultConnection($database);      
+
         try {
             return response()->json([
                 'status' => 'success',
